bcb513d6bb713737125976aa8555c47b8f236666
Jana Jarecki <jarecki.jana@gmail.com>
2019-10-02 17:15:59 +0200

    wip

--- analyses/code/fig_setup.R
+++ analyses/code/fig_setup.R
@@ -22,10 +22,11 @@ order_evidence <- function(id, evidence) {
   theme_update(
     legend.position = "right",
     legend.direction = "vertical",
-    aspect.ratio = 1.6,
     legend.box.background = element_rect(size = 0.4, color = "black", fill = NA),
     legend.margin = margin(rep(0.2,4), unit="lines"),
     legend.key.width = unit(1, "lines"),
     legend.box.margin = margin(rep(0.3,4), unit = "lines"),
-    panel.spacing.x = unit(2, "lines")
+    legend.spacing.x = unit(0.4, "lines"),
+    legend.text = element_text(margin = margin(r = 0.4, unit = "lines")),
+    legend.key = element_rect(color = "grey", size = 0.2)
     )
