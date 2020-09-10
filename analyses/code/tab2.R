33ffeee9790977bff3c42c36c4999c60e5bef884
Jana Jarecki <jarecki.jana@gmail.com>
2020-09-10 13:19:41 +0200

    wip

--- analyses/code/tab2.R
+++ analyses/code/tab2.R
@@ -6,22 +6,25 @@ pacman::p_load(data.table, BayesFactor, brms)
 pacman::p_load_gh("crsh/papaja@devel")
 # Set working directory to this file location
 
-# Load data
+
+# Load data ----------------------------------------------------------------
 study <- 1
 path <- sub("x", study, "../../data/processed/studyx.csv")
 d <- fread(path,
   select = c("id", "condition", "samplesizecat", "samplesize", "gambletype", "value", "gambleid"),
   colClasses = list(factor = c("id", "condition"), double = c("samplesize", "gambleid")))
 d[condition=="description", samplesizecat := "--"]
-# d[, samplesizecat := factor(samplesizecat, levels = c("xs", "s", "m", "l", "--"))]
 
-M <- d[, .(
-  Med = median(value),
-  M = mean(value)
-  ),
+
+# Compute descriptive statistics ----------------------------------------------
+# Mean and Median by conditions
+M <- d[, .(Mdn = median(value), M = mean(value)),
   by = .(condition, gambleid, samplesizecat, samplesize)]
+# Description-experience gap
 M[, `D-E` := rep(M[condition == "description"], .N) - c(M[condition == "experience"], NA), by = .(gambleid)]
 
+
+# Compute inferential statistics ----------------------------------------------
 get_bf10 <- function(g, s, niter = 1000) {
   bf10 <- anovaBF(
     formula = value ~ condition + id,
@@ -30,32 +33,31 @@ get_bf10 <- function(g, s, niter = 1000) {
     iterations = niter)
   return(extractBF(bf10, onlybf = TRUE))
 }
-
-# Run get_bf10 by each gamble ID x sample-size
+set.seed(42)
 M[condition == "experience", `D-E BF10` := get_bf10(g = gambleid[1], s = samplesizecat[1]), by = .(gambleid, samplesizecat)]
 
+
+# Make Table ------------------------------------------------------------------
+options(papaja.na_string = "--")
+# Cosmetics for the table
 M[, condition := ifelse(condition == "experience", "E", "D")]
 M[, gambleid := as.numeric(gambleid)]
 M[, samplesize := as.numeric(samplesize)]
 M[, samplesizecat := factor(samplesizecat, levels = c("xs", "s","m","l","--"))]
 M <- M[order(gambleid, samplesizecat)]
 setcolorder(M, c(2,1,3:8))
-
 M[, gambleid := paste0(gambleid, rep(c(" (\\$-bet)", " (p-bet)"), each = 15))]
 M <- split(M, by = "gambleid", keep.by = F)
 names(M) <- paste("Gamble ID", names(M))
-
-# Make Table ------------------------------------------------------------------
 # format the Bayes Factor such that BF > 1000 is displayed as > 1000
 M <- lapply(M, function(x) cbind(x[, 1:6], replace(round(x[, 7], 0), round(x[, 7], 0) > 1000, ">1000")))
 
-options(papaja.na_string = "--")
 tab2 <- apa_table(M
     , caption = "Valuations of Gambles in Study 1"
     , col.names = c("Condition", 'Sample size category', 'Sample size', '\\textit{Med}', '\\textit{M}', 'D--E', 'D--E:$BF\\textsubscript{10}$')
     , align = c('l', rep('c', 4), 'r', 'r')
     , digits = c(0,0,0,2,2,2,0)
-    , note = '\\textit{M} = mean, \\textit{Med} = median, D--E = difference between mean description-based valuations and experience-based valuations, $BF\\textsubscript{10}$ = Bayes Factor quantifying the evidence for a linear model $\\mathrm{M}\\textsubscript{1}$ predicting that valuations differ between description and experience over a linear model $\\mathrm{M}\\textsubscript{0}$ predicting no such differences; both models models contain a by-participant random effect. Gambles IDs 1, 2, and 3 are \\$-bets; Gamble IDs 4, 5, and 6 are p-bets.'
+    , note = '\\textit{M} = mean, \\textit{Mdn} = median, D--E = difference between mean description-based valuations and experience-based valuations, $BF\\textsubscript{10}$ = Bayes Factor quantifying the evidence for a linear model $\\mathrm{M}\\textsubscript{1}$ predicting that valuations differ between description and experience over a linear model $\\mathrm{M}\\textsubscript{0}$ predicting no such differences; both models models contain a by-participant random effect. Gambles IDs 1, 2, and 3 are \\$-bets; Gamble IDs 4, 5, and 6 are p-bets.'
     , escape = FALSE
     )
 
