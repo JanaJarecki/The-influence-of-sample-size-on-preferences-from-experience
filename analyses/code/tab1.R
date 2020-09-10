f30dec86a8fa6e9c853d31dfccdcac94ee0fc858
Jana Jarecki <jarecki.jana@gmail.com>
2020-09-10 12:44:44 +0200

    wip

--- analyses/code/tab1.R
+++ analyses/code/tab1.R
@@ -17,7 +17,7 @@ tab <- dcast(tab, ... ~ samplesizecat, value.var = "samplesize")
 tab <- apa_table(tab,
   col.names = c("Gamble ID", "Type", "X", "Pr", "EV", "xs", "s", "m", "l"),
   align = c("l", "l", rep("c", 7)),
-  note = "\textit{X} = gain in Swiss Fr., \textit{Pr} = probability of gain, \textit{EV} = expected value, \textit{Sample Size} = total number of observations in the experience condition categorized as \textit{xs} = extra small, \textit{s} = small, \textit{m} = medium, \textit{l} = large. The probability is expressed as the ratio of the relative frequency of the number of gain observations to the number of observations in the smallest sample size category (xs) of this gamble, namely 1/5, 1/6, 1/7, 4/5, 5/6, and 6/7 for gamble IDS 1 through 6, respectively.",
+  note = "\\textit{X} = gain in Swiss Fr., \\textit{Pr} = probability of gain, \\textit{EV} = expected value, \\textit{Sample Size} = total number of observations in the experience condition categorized as \\textit{xs} = extra small, \\textit{s} = small, \\textit{m} = medium, \\textit{l} = large. The probability is expressed as the ratio of the relative frequency of the number of gain observations to the number of observations in the smallest sample size category (xs) of this gamble, namely 1/5, 1/6, 1/7, 4/5, 5/6, and 6/7 for gamble IDS 1 through 6, respectively.",
   caption = "Gambles and Sample Sizes Used in Studies 1 and 2{table:Lotteries}}",
   placement = "H",
   col_spanners = list("Sample Size" = c(6,9))
