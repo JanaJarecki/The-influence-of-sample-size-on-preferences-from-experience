c287d90ec03e0bad20e7305b3aa0e981cc7c9334
Jana Jarecki <jarecki.jana@gmail.com>
2020-09-10 13:01:36 +0200

    update

--- analyses/code/tab2.R
+++ analyses/code/tab2.R
@@ -1,13 +1,12 @@
 # ==========================================================================
 # Table 2 for study 1
+# Author: Jana Jarecki
 # ==========================================================================
-library(data.table)
-library(BayesFactor)
-library(brms)
-
+pacman::p_load(data.table, BayesFactor, brms)
+# Set working directory to this file location
 
 # Load data
-# study <- 1
+study <- 1
 path <- sub("x", study, "../../data/processed/studyx.csv")
 d <- fread(path,
   select = c("id", "condition", "samplesizecat", "samplesize", "gambletype", "value", "gambleid"),
@@ -22,11 +21,6 @@ M <- d[, .(
   by = .(condition, gambleid, samplesizecat, samplesize)]
 M[, `D-E` := rep(M[condition == "description"], .N) - c(M[condition == "experience"], NA), by = .(gambleid)]
 
-# get_bf10 <- function(g, s) {
-#   fit <- brm(value ~ condition + (1|id), 
-#             data = d[gambleid == g & samplesizecat %in% c(s, "--")])
-# }
-
 get_bf10 <- function(g, s, niter = 1000) {
   bf10 <- anovaBF(
     formula = value ~ condition + id,
@@ -35,6 +29,7 @@ get_bf10 <- function(g, s, niter = 1000) {
     iterations = niter)
   return(extractBF(bf10, onlybf = TRUE))
 }
+
 # Run get_bf10 by each gamble ID x sample-size
 M[condition == "experience", `D-E BF10` := get_bf10(g = gambleid[1], s = samplesizecat[1]), by = .(gambleid, samplesizecat)]
 
@@ -48,3 +43,21 @@ setcolorder(M, c(2,1,3:8))
 M[, gambleid := paste0(gambleid, rep(c(" (\\$-bet)", " (p-bet)"), each = 15))]
 M <- split(M, by = "gambleid", keep.by = F)
 names(M) <- paste("Gamble ID", names(M))
+
+# Make Table ------------------------------------------------------------------
+# format the Bayes Factor such that BF > 1000 is displayed as > 1000
+M <- lapply(M, function(x) cbind(x[, 1:6], replace(round(x[, 7], 0), round(x[, 7], 0) > 1000, ">1000")))
+apa_table(M
+    , caption = "Valuations of Gambles in Study 1"
+    , col.names = c("Condition", 'Sample size category', 'Sample size', '\\textit{Med}', '\\textit{M}', 'D--E', 'D--E:$BF\\textsubscript{10}$')
+    , align = c('l', rep('c', 4), 'r', 'r')
+    , digits = c(0,0,0,2,2,2,0)
+    , note = '\\textit{M} = mean, \\textit{Med} = median, D--E = difference between mean description-based valuations and experience-based valuations, $BF\\textsubscript{10}$ = Bayes Factor quantifying the evidence for a linear model $\\mathrm{M}\\textsubscript{1}$ predicting that valuations differ between description and experience over a linear model $\\mathrm{M}\\textsubscript{0}$ predicting no such differences; both models models contain a by-participant random effect. Gambles IDs 1, 2, and 3 are \\$-bets; Gamble IDs 4, 5, and 6 are p-bets.'
+    , escape = FALSE
+    )
+
+
+# Append results for Rtex manuscript ------------------------------------------
+R <- readRDS("../../manuscript/results4tex1.rds")
+R$tab2 <- M
+saveRDS(R, file = "../../manuscript/results4tex1.rds", version = 2)
